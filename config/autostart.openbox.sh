8a2245e81825db3639f2c4c214563be17ccdb041
Tomasz Wyderka <wyderkat@cofoh.com>
2012-10-22 12:33:49 +0700

    Makefile for playwm

--- config/autostart.openbox.sh
+++ config/autostart.openbox.sh
@@ -1,4 +1,8 @@
 xrandr --output LVDS1 --off
 xrandr --output VGA1 --mode 1920x1080
 feh --bg-center  ~/.background/trawa.jpg
-fbpanel &
+xcompmgr &
+sleep 1 # xcompmgr has to be started before next applications
+tint2 -c ~/.config/tint2/launch.bar.tint2rc &
+tint2 -c ~/.config/tint2/task.bar.tint2rc &
+volumeicon &
