6debc5bc51e99b32af15d1c2647887ca97530da2
Tomasz Wyderka <wyderkat@cofoh.com>
2012-11-16 23:25:42 +0700

    logs

--- config/autostart.openbox.sh
+++ config/autostart.openbox.sh
@@ -1,8 +1,9 @@
+LOG=~/.playwm/log
 feh --bg-center  /usr/lib/playwm/image/wallpaper19201080.jpg
-xcompmgr &
+xcompmgr   &> $LOG/xcompmgr &
 sleep 1 # xcompmgr has to be started before next applications with delay
-tint2 -c ~/.playwm/launch.bar.tint2rc &
-tint2 -c ~/.playwm/task.bar.tint2rc &
+tint2 -c ~/.playwm/launch.bar.tint2rc  &> $LOG/launch.bar &
+tint2 -c ~/.playwm/task.bar.tint2rc  &> $LOG/task.bar  &
 xrdb -merge ~/.playwm/terminal.Xresources
 volumeicon &
 nm-applet&
