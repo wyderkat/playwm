714345b3e5bb3251338e99dd4de9dc5f43e4b049
Tomasz Wyderka <wyderkat@cofoh.com>
2012-11-16 23:30:11 +0700

    logs

--- config/.patch.autostart.openbox.sh
+++ config/.patch.autostart.openbox.sh
@@ -2,8 +2,8 @@
 > xrandr --output LVDS1 --off
 > xrandr --output VGA1 --mode 1920x1080
 7a10,14
-> workrave &
-> mail-notification &
+> workrave &> $LOG/workrave &
+> mail-notification &> $LOG/mail-notification &
 > # radiotray doesn't want to start, even after long sleep
 > # but only from lightdm. from startx is OK
-> radiotray > /home/tom/a.log 2>&1 &
+> radiotray &> $LOG/radiotray  &
