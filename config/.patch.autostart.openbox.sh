4ad30d7ee47e703006294075c492fdfc4b09f364
Tomasz Wyderka <wyderkat@cofoh.com>
2012-11-21 15:43:07 +0700

    icon theme dep

--- config/.patch.autostart.openbox.sh
+++ config/.patch.autostart.openbox.sh
@@ -1,9 +1,7 @@
 0a1,2
 > xrandr --output LVDS1 --off
 > xrandr --output VGA1 --mode 1920x1080
-7a10,14
+7a10,12
 > workrave &> $LOG/workrave &
 > mail-notification &> $LOG/mail-notification &
-> # radiotray doesn't want to start, even after long sleep
-> # but only from lightdm. from startx is OK
 > radiotray &> $LOG/radiotray  &
