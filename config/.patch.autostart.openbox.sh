09ef3acc601dbfe2ef5742175ffaa0c0bb07a970
Tomasz Wyderka <wyderkat@cofoh.com>
2012-11-29 13:23:31 +0700

    small

--- config/.patch.autostart.openbox.sh
+++ config/.patch.autostart.openbox.sh
@@ -1,7 +1,9 @@
 0a1,2
 > xrandr --output LVDS1 --off
 > xrandr --output VGA1 --mode 1920x1080
-7a10,12
+17a20
+> setxkbmap -option grp:switch,grp:shifts_toggle,grp_led:scroll pl,th
+27a31,33
 > workrave &> $LOG/workrave &
 > mail-notification &> $LOG/mail-notification &
 > radiotray &> $LOG/radiotray  &
